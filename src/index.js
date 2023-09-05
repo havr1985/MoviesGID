5ef1bcc66e2669a5a5fcb23f77832d0acb3c0607
havr1985 <gavrtel1985@gmail.com>
2023-09-06 00:31:58 +0300

    work with API

--- src/index.js
+++ src/index.js
@@ -1,19 +1,71 @@
-import axios from 'axios';
+import { filmAPI } from "./js/films-api";
+import { markUpMovie } from "./js/markup";
+import { markUpPeople } from "./js/markup";
+import { markUpTv } from "./js/markup";
+import SlimSelect from 'slim-select'
 
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZGViMWNlYjdkMDA2NzJlNGYyZWZlMDI2MTQwNWNjNiIsInN1YiI6IjY0ZjczOTllZmZjOWRlMDBhYzRmMTg5MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.arW0TovcOo_eqnVO6td0Nh7eaWCU_GXyNFEcZBWV-_o'
+const select = document.querySelector('.category-select');
+const container = document.querySelector('.container');
+const target = document.querySelector('.js-guard');
+const api = new filmAPI();
+
+new SlimSelect({
+    select: '#selectElement',
+    settings: {
+    placeholderText: 'Please select a category',
   }
-};
-async function loadAPI() {
+   
+})
 
+select.addEventListener('change', onChange);
+
+function onChange(event) {
+    container.innerHTML = '';
+    api.PAGE = 1;
+    let val = event.target.value;
+    if (!val) {
+        return;
+    }
+    api.CHECKED = val;
+    loadCards();
+}
+
+var options = {
+  rootMargin: "300px",
+};
+const observer = new IntersectionObserver(handlerLoadMore, options);
+
+async function loadCards() {
     try {
-        const response = await axios.get('https://api.themoviedb.org/3/trending/person/week?',options);
-        console.log(response);
+        const cards = await api.loadAPI();
+        if (api.CHECKED === 'movie') {
+            container.insertAdjacentHTML('beforeend', markUpMovie(cards.results));
+        };
+        if (api.CHECKED === 'person') {
+            container.insertAdjacentHTML('beforeend', markUpPeople(cards.results));
+        };
+        if (api.CHECKED === 'tv') {
+            container.insertAdjacentHTML('beforeend', markUpTv(cards.results));
+        }
+        observer.observe(target);
+        if (api.PAGE >= cards.total_pages) {
+                observer.unobserve(target);
+            };
+        console.log(api.PAGE);
     } catch (error) {
-        console.error(error);
+        console.log(error)
     }
 }
-loadAPI()
+
+function handlerLoadMore(entries) {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            console.log(entries)
+            api.PAGE += 1;
+            loadCards();
+        }
+    })
+};
+
+
+
---
+++ src/js/films-api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZGViMWNlYjdkMDA2NzJlNGYyZWZlMDI2MTQwNWNjNiIsInN1YiI6IjY0ZjczOTllZmZjOWRlMDBhYzRmMTg5MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.arW0TovcOo_eqnVO6td0Nh7eaWCU_GXyNFEcZBWV-_o'
+  }
+};
+
+export class filmAPI{
+
+    URL = 'https://api.themoviedb.org/3/trending/';
+    CHECKED = 'person';
+    DATES = 'week';
+    PAGE = 1;
+    
+async loadAPI() {
+
+    const response = await axios.get(`${this.URL}/${this.CHECKED}/${this.DATES}?page=${this.PAGE}`,options);
+    const data = response.data;
+    return data;
+    
+}
+}
